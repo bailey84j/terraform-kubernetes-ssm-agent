bcffe9a2ba0500990d7b640a686b55be31de2c01
james-bailey <james.bailey@fisglobal.com>
2022-01-24 16:50:34 +0000

    simple fixes

--- variables.tf
+++ variables.tf
@@ -1,12 +1,18 @@
 variable "name" {
-  description = "The name of the xray daemonset"
+  description = "The name of the ssm daemonset"
   type        = string
   default     = "xray"
 
 }
 
+variable "create_namespace" {
+  description = "Determines whether to create a new kubernetes namespace for the deployment"
+  type        = bool
+  default     = false
+}
+
 variable "namespace" {
-  description = "The namespace to place the xray daemonset"
+  description = "The namespace to place the daemonset"
   type        = string
   default     = "kube-system"
 
